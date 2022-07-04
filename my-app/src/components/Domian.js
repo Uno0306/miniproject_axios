c910ed65bd78254931fef133a066b864c9899dcc
Uno0306 <82020768+Uno0306@users.noreply.github.com>
2022-07-04 09:38:48 +0900

    create domain

--- my-app/src/components/Domian.js
+++ my-app/src/components/Domian.js
@@ -1,8 +1,18 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router';
 
-function Domain(){
-    return;
+function Domain() {
+  return (
+    <div>
+      <Link to={'/selectform'}>
+        <p>select</p>
+      </Link>
+      <Link to='/insertform'>insert</Link>
+      <Link to='/updateform'>update</Link>
+      <Link to='/deleteform'>delete</Link>
+    </div>
+  );
 }
 
 export default Domain;
